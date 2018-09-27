06f1032741c294e566bc3cf84d0771244c4c2ed8
Pedro Alexandre <retiarus@gmail.com>
2018-09-27 14:36:30 -0300

    add window variable to ue with the function smooth in preprocessing

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ local_tecmap_txt = os.path.join(default_location, 'data/tecmap_txt')
 local_data = os.path.join(default_location, 'data')
 local_s4 = os.path.join(default_location, 'data_s4')
 local_s4_pre = os.path.join(default_location, 'data_s4_pre')
+window = 9
 
 group_1 = ['bhz', '30', '25', 'sj2']
 
